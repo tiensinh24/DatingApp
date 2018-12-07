6a25f3364514e5f101624f23ffa42df4f9ae1f03
tiensinh24 <nguyenphucan2502@gmail.com>
2018-12-07 16:24:18 +0700

    End of Section 8

--- DatingApp.API/Dtos/UserForListDto.cs
+++ DatingApp.API/Dtos/UserForListDto.cs
@@ -1,17 +1,13 @@
 using System;
-using System.Collections.Generic;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForListDto
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-
+        public string Username { get; set; }     
         public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -20,7 +16,7 @@ namespace DatingApp.API.Models
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string PhotoUrl { get; set; }               
         
-        public ICollection<Photo> Photos { get; set; }
     }
 }
